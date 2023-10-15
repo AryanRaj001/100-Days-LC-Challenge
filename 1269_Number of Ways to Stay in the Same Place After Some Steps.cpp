8eac609c46793487c27c7477491fc1b86b451a25
Aryan Raj <109361430+AryanRaj001@users.noreply.github.com>
2023-10-15 21:34:36 +0530

    Create 1269_Number of Ways to Stay in the Same Place After Some Steps.cpp

---
+++ 1269_Number of Ways to Stay in the Same Place After Some Steps.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    long long dp(int step, int index, int arrLen, const int MOD, std::vector<std::vector<long long>>& memo) {
+        if (step == 0) {
+            return (index == 0) ? 1 : 0;
+        }
+        if (step < 0 || index < 0 || index >= arrLen) {
+            return 0;
+        }
+        if (memo[step][index] != -1) {
+            return memo[step][index];
+        }
+        
+        long long ways = dp(step - 1, index, arrLen, MOD, memo) % MOD;
+        ways += dp(step - 1, index - 1, arrLen, MOD, memo) % MOD;
+        ways += dp(step - 1, index + 1, arrLen, MOD, memo) % MOD;
+        
+        memo[step][index] = ways % MOD;
+        return ways % MOD;
+    }
+
+    int numWays(int steps, int arrLen) {
+        const int MOD = 1000000007;
+        vector<vector<long long>> memo(steps + 1, vector<long long>(min(arrLen, steps) + 1, -1));
+        
+        return dp(steps, 0, arrLen, MOD, memo);
+    }
+};
